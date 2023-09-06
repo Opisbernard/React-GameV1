9fc1a692474b85ba3ce7c9094851a23c542d52f1
Opisbernard <bernard112399@gmail.com>
2023-09-06 16:28:12 +0800

    removing file

--- src/components/molecules/Footer/Footer.js
+++ src/components/molecules/Footer/Footer.js
@@ -3,7 +3,7 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div className="author">Created by {new Date().getFullYear()} {" "}
-      <a href="https://github.com/Opisbernard" target="_blank" rel="noreferrer" className="copyright-link">Bernard Opis</a>
+      <a href="https://github.com/Opisbernard" target="_blank" rel="noreferrer" >Bernard Opis</a>
     </div>
   )
 }
